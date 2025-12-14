6cfcdbba58c3130ef199e72eaa08be0ba3f337b3
Ayan-Bain <ayan.bain@iitg.ac.in>
2025-12-14 21:13:39 +0530

    Finally Done

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-
-const answerElement = document.getElementsByClassName('answer');
-console.log(answerElement);
-
-const answer = [];
-
-for( i=0;i<answerElement.length ;i++) {
-    answerElement[i].style.height = 'auto';
-    answer[i] = answerElement[i].clientHeight + ""+'px';
-    answerElement[i].style.height = '0px';
-}
-
-console.log(answer);
-for(i=0;i<answer.length;i++) {
-    console.log(answer[i]);
-}
-
 const question = document.querySelectorAll(".accordion .content-container");
 console.log(question);
 
 for(i=0;i<question.length;i++) {
     question[i].addEventListener('click', function() {
         this.classList.toggle('active');
-        console.log(this.children[1]);
-        console.log(answer[i]);
-        this.children[1].style.height = sth;
-        console.log(this.children[1]);
+        const answer = this.querySelector('.answer');
+        if (this.classList.contains('active')) {
+            answer.style.height = answer.scrollHeight + 'px';
+        }
+        else {
+            answer.style.height = '0px';
+        }
+
     })
 }
 
